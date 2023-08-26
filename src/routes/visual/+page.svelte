e7349d031b88d89db07e02155a99f6c103e7e04a
shamim09 <shamimahmed903@gmail.com>
2023-08-26 03:45:18 +0200

    edit home page

--- src/routes/visual/+page.svelte
+++ src/routes/visual/+page.svelte
@@ -3,9 +3,4 @@
   import GeneOverview from './GeneOverview.svelte';
 </script>
 
-<h1>Details of Assignment</h1>
-<p>The E.coli genome consists of a single circular chromosome. Each gene has a start and stop position on that chromosome, as well
-as a "strand". This strand indicates in which direction the gene points: clockwise or counter-clockwise....</p>
 
-<!-- Display the gene visual -->
-<GeneOverview />
